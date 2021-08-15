f0fc8e399557037ba4f7d9d77f91feedb8746ee2
Julie Vandard <julie.vandard@hey.com>
2021-08-15 11:44:53 +0200

    Changement d'un flex pour un grid pour les cards

---
+++ app.js
@@ -0,0 +1,9 @@
+const burgerMenu = document.querySelector('.menu-burger');
+const mobile-nav = document.querySelector('.nav-links');
+
+burgerMenu.addEventListener('click', () = {
+    mobile-nav.classList.toggle('open');
+    burgerMenu.classList.toggle('open');
+});
+
+   
